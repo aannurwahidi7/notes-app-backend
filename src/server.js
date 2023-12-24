0842610621977607d67b7b343809914827454004
Aan Nur Wahidi <aannurwahidi117@gmail.com>
2023-12-24 21:24:31 +0700

    ReStructure and add Authentication

--- src/server.js
+++ src/server.js
@@ -1,18 +1,28 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
-// const routes = require('./api/notes/routes');
+
+// notes
 const notes = require('./api/notes');
 const NotesService = require('./api/services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
+// users
 const users = require('./api/users');
 const UserService = require('./api/services/postgres/UserService');
 const UsersValidator = require('./validator/users');
 
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./api/services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+const ClientError = require('./exceptions/ClientError');
+
 const init = async () => {
     const notesService = new NotesService();
     const userService = new UserService();
+    const authenticationsService = new AuthenticationsService();
 
     const server = Hapi.server({
         port: process.env.PORT,
@@ -39,8 +49,45 @@ const init = async () => {
                 validator: UsersValidator,
             },
         },
-
+        {
+            plugin: authentications,
+            options: {
+                authenticationsService,
+                userService,
+                tokenManager: TokenManager,
+                validator: AuthenticationsValidator,
+            },
+        },
     ]);
+
+    server.ext('onPreResponse', (request, h) => {
+        const { response } = request;
+    
+        if (response instanceof Error) {
+          // penanganan error secara internal
+            if (response instanceof ClientError) {
+                const newResponse = h.response({
+                    status: 'fail',
+                    message: response.message,
+                });
+                newResponse.code(response.statusCode);
+                return newResponse;
+            }
+    
+            if (!response.isServer) {
+                return h.continue;
+            }
+    
+            const newResponse = h.response({
+                status: 'error',
+                message: 'terjadi kegagalan pada server kami',
+            });
+            newResponse.code(500);
+            return newResponse;
+        }
+
+        return h.continue;
+    });
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
